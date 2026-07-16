096ca3b50c1143c90c5595cac2494e9090898f99
Himanshu Chhatrala <hiamsnhu.chhatrala@xpertnest.com>
2026-07-16 22:56:46 +0530

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { helmet } from 'helmet';
+import helmet from 'helmet';
 import cors from 'cors';
 import compression from 'compression';
 import morgan from 'morgan';
